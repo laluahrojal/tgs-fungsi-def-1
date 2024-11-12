acf8dba5db62a8b2bb20bf6e192a18e5ff12fa11
laluahrojal <lahrojal@gmail.com>
2024-11-12 16:06:01 +0800

    tgs fungsi.py

---
+++ tgs fungsi.py
@@ -0,0 +1,8 @@
+https://drive.google.com/file/d/1aTxj4wheCUUoay_rI3FT3Npu2tV0rmyW/view?usp=sharing
+
+def fungsiku (x):
+    return (10*x)-20
+    print()
+a = float(input("masukkan angka:"))
+hasil_fx =fungsiku(a)
+print("hasil dari funfsi f(x) = (10*x)-200 adalah: ",hasil_fx)
